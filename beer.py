d8de0e30c8f87a77ea1f0109f9a3573152f5620c
alevie1 <112024083+alevie1@users.noreply.github.com>
2023-01-13 06:37:40 +0000

    finished code without drunkenness

--- beer.py
+++ beer.py
@@ -1,11 +1,12 @@
 #Prompt for how many bottles of beer are, in fact, on the wall
-bottle_number = int(input("How many bottles of beer are on the wall?\n"))
+bottle_number = int(input('How many bottles of beer are on the wall?\n'))
 #Cry if it is zero?
 if bottle_number < 1:
 	print("\n*incoherent sobbing at the sad lack of beer*\n")
 #Make it sing every verse, make sure it doesn't say 'bottles' for one bottle
+print('')
 while bottle_number > 0:
-	if bottle_number = 1:
+	if bottle_number == 1:
 		print(f'{bottle_number} bottle of beer on the wall')
 		print(f'{bottle_number} bottle of beer')
 		print(f'Take it down,\nPass it around,')

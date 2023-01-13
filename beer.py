facd1a76c1980aeb6ffa607604c90e98ecb5812a
alevie1 <112024083+alevie1@users.noreply.github.com>
2023-01-13 06:51:25 +0000

    wheeee

--- beer.py
+++ beer.py
@@ -1,5 +1,6 @@
 #Prompt for how many bottles of beer are, in fact, on the wall
 bottle_number = int(input('How many bottles of beer are on the wall?\n'))
+drunk = 0
 #Cry if it is zero?
 if bottle_number < 1:
 	print("\n*incoherent sobbing at the sad lack of beer*\n")
@@ -7,15 +8,19 @@ if bottle_number < 1:
 print('')
 while bottle_number > 0:
 	if bottle_number == 1:
-		print(f'{bottle_number} bottle of beer on the wall')
-		print(f'{bottle_number} bottle of beer')
-		print(f'Take it down,\nPass it around,')
-		bottle_number = (bottle_number - 1)
-		print(f'{bottle_number} bottles of beer on the wall\n')
+		line1 = f'{bottle_number} bottle of beer on the wall'
+		line2 = f'{bottle_number} bottle of beer'
+		line3 = f'Take it down,\nPass it around,'
+		bottle_number = bottle_number - 1
+		line4 = f'{bottle_number} bottles of beer on the wall'
 	else:
-		print(f'{bottle_number} bottles of beer on the wall')
-		print(f'{bottle_number} bottles of beer')
-		print(f'Take one down,\nPass it around,')
-		bottle_number = (bottle_number - 1)
-		print(f'{bottle_number} bottles of beer on the wall\n')
+		line1 = f'{bottle_number} bottles of beer on the wall'
+		line2 = f'{bottle_number} bottles of beer'
+		line3 = f'Take one down,\nPass it around,'
+		bottle_number = bottle_number - 1
+		line4 = f'{bottle_number} bottles of beer on the wall'
+	drunk += 1
+	#if drunk > 5:
+		#
+	print(f'{line1}\n{line2}\n{line3}\n{line4}')
 #Add 'drunkenness' - substitute random characters into the string?

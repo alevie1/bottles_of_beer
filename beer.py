b34d35ebd9045460f09b039fe9be08de3db2ade5
alevie1 <24alevie@go.dsdmail.net>
2023-01-26 13:21:31 -0700

    fixed grammar

--- beer.py
+++ beer.py
@@ -14,7 +14,10 @@ while bottle_number > 0:
 	else:
 		part1 = f'{bottle_number} bottles of beer on the wall,\n{bottle_number} bottles of beer,\nTake one down,\nPass it around,\n'
 		bottle_number = bottle_number - 1
-		part2 = f'{bottle_number} bottles of beer on the wall'
+		if bottle_number == 1:
+			part2 = f'{bottle_number} bottle of beer on the wall'
+		else:
+			part2 = f'{bottle_number} bottles of beer on the wall'
 	drunk += 1
 	#if drunk > 5:
 		#finish this part later
